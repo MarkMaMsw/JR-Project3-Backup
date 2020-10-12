a03b6ecaa00fd9094f9aa9b958638c871585faf2
spcidealacm <spcidealacm@gmail.com>
2020-10-12 20:43:46 +1100

    run

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Users from "./pages/Users/Users";
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+            <Route path="/about" component={About}/>
+            <Route path="/users" component={Users}/>
+            <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function About() {
+  return (
+    <p>
+        about
+    </p>
+  );
+}
+
+export default About;
