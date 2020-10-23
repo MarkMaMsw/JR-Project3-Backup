fc45f5a96fa4e1d4b4962b018c2529211792284f
Sophia Sun <sunjiajia0704@gmail.com>
2020-10-23 16:10:57 +1100

    Version1.2 - Easy Version

--- src/components/Display/ProductRectangleCard1/index.js
+++ src/components/Display/ProductRectangleCard1/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Style from "./style.module.scss"
 import { withRouter } from 'react-router-dom';
 import ReactDOM from "react-dom";
-
+import StarIcon from '@material-ui/icons/Star';
 
 class ProductRectangleCard1 extends React.Component{
     constructor(){
@@ -21,17 +21,17 @@ class ProductRectangleCard1 extends React.Component{
         <div className = "container">
             <div className = "row">
                 <div className = "col-md-3 col-xs-6 col-sm-4">
-                    <div className = {Style.productContainer} >
-                        <img ref={this.imgPath} src={this.props.imgPath} alt="shoes" />
-                        <div>
+                    <div className = {Style.rec_productContainer} >
+                        <img ref={this.imgPath} src={this.props.imgPath} alt="shoes" className = {Style.rec_Product}/>
+                        <div className = {Style.rec_productInfo}>
                             <h4>{name}</h4>
-                        </div>
-                        <div>
-                            <h4>$ {price}</h4>
-                            <div>
-                                <h4>{rate}</h4>
-                            </div>
-                        </div>
+                                <div className = {Style.rec_productExtraInfo}>
+                                    <div className = {Style.rec_productRate}>                                       
+                                        <h4 className = {Style.rec_productRateText}><StarIcon color = "secondary"/>{rate}</h4>
+                                    </div>
+                                    <h4 className = {Style.rec_productPrice}>$ {price}</h4>
+                                </div>
+                        </div>         
                     </div>
                 </div>
             </div>
