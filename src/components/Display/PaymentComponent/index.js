0be9705b31e67d556acdf15b69982848e8a43f9f
Chenxi Li <sunshinerxx@gmail.com>
2020-10-25 20:15:23 +1000

    mofity repsonsive desgin for subtotal price and cards

--- src/components/Display/PaymentComponent/index.js
+++ src/components/Display/PaymentComponent/index.js
@@ -67,33 +67,35 @@ class PaymentComponent extends React.Component{
             nameOnCard } = this.state
         const { inputStyle, labelStyle } = this.props
         return <div  className={Style["payment"]}>
-                <div className={Style["payment__cart"]}>
-                <div className={Style["payment__cart--title"]}>
-                    <h2>Shopping Cart</h2>
-                </div>
-                <div className={Style["payment__cart--body"]}>
-                    <CategorySquareCard1 
-                        name = "Ultraboost 20 Shoes"
-                        imgPath={require("../../../images/shoes/ultraboost-20-shoes.png")}
-                        price = "340"
-                        rate = "3.4"
-                        numofColor = "8"
-                    />
-                    <CategorySquareCard1 
-                        name = "Ultraboost 20 Shoes"
-                        imgPath={require("../../../images/shoes/ultraboost-20-shoes.png")}
-                        price = "450"
-                        rate = "4.6"
-                        numofColor = "3"
-                        newContainerStyle = "background: rgba(255, 148, 56, 0.2)"
-                    />
-                    <CategorySquareCard1 
-                        name = "Ultraboost 20 Shoes"
-                        imgPath={require("../../../images/shoes/ultraboost-20-shoes.png")}
-                        price = "330"
-                        rate = "3.8"
-                        numofColor = "2"
-                    />
+            <div className={Style["payment__cart"]}>
+                <div className={Style["payment__cart--container"]}>
+                    <div className={Style["payment__cart--title"]}>
+                        <h2>Shopping Cart</h2>
+                    </div>
+                    <div className={Style["payment__cart--body"]}>
+                        <CategorySquareCard1 
+                            name = "Ultraboost 20 Shoes"
+                            imgPath={require("../../../images/shoes/ultraboost-20-shoes.png")}
+                            price = "340"
+                            rate = "3.4"
+                            numofColor = "8"
+                        />
+                        <CategorySquareCard1 
+                            name = "Ultraboost 20 Shoes"
+                            imgPath={require("../../../images/shoes/ultraboost-20-shoes.png")}
+                            price = "450"
+                            rate = "4.6"
+                            numofColor = "3"
+                            newContainerStyle = "background: rgba(255, 148, 56, 0.2)"
+                        />
+                        <CategorySquareCard1 
+                            name = "Ultraboost 20 Shoes"
+                            imgPath={require("../../../images/shoes/ultraboost-20-shoes.png")}
+                            price = "330"
+                            rate = "3.8"
+                            numofColor = "2"
+                        />
+                    </div>
                 </div>
                 <div className={Style["payment__subtotal--container"]}>
                     <div className={Style["payment__subtotal--content"]}>
