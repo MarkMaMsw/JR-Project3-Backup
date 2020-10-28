c800cdece72e7f1c1a78cfae45c2b97860562e75
Sophia Sun <sunjiajia0704@gmail.com>
2020-10-28 15:46:30 +1100

    Version3.5 - Add img & button link

--- src/components/Display/MainRectangleCard1/index.js
+++ src/components/Display/MainRectangleCard1/index.js
@@ -7,29 +7,59 @@ import Button from '@material-ui/core/Button';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
 
-class MainRectangleCard1 extends React.Component{
+class _MainRectangleCard1 extends React.Component{
     constructor(){
         super();
         this.imgPath = React.createRef();
+        this.rec_productContainer = React.createRef();
+        this.imgContainer = React.createRef();
+        this.mainrec_Name = React.createRef();
+        this.mainrec_Price = React.createRef();
+        this.mainrec_Button = React.createRef();
         this.state = {
             name: "Product Name",
             price: 0, 
         }
     }
 
+    componentDidMount(){
+        const { newrec_ContainerStyle, newimgContainerStyle, newrec_NameStyle, newrec_PriceStyle, newrec_ButtonStyle} = this.props;
+        const mainpro_container = ReactDOM.findDOMNode(this.rec_productContainer.current);
+        const mainimg_container = ReactDOM.findDOMNode(this.imgContainer.current);
+        const mainpro_name = ReactDOM.findDOMNode(this.mainrec_Name.current);
+        const mainpro_price = ReactDOM.findDOMNode(this.mainrec_Price.current);
+        const mainpro_button = ReactDOM.findDOMNode(this.mainrec_Button.current);
+
+        if (newrec_ContainerStyle) mainpro_container.style.cssText = newrec_ContainerStyle;
+        if (newimgContainerStyle) mainimg_container.style.cssText = newimgContainerStyle;
+        if (newrec_NameStyle) mainpro_name.style.cssText = newrec_NameStyle;
+        if (newrec_PriceStyle) mainpro_price.style.cssText = newrec_PriceStyle;
+        if (newrec_ButtonStyle) mainpro_button.style.cssText = newrec_ButtonStyle;
+    }
+
+    buttonLink = () => {
+        const { buttonLink } = this.props;
+        if (buttonLink) this.props.history.push(buttonLink);
+    }
+
+    imgLink = () => {
+        const { imgLink } = this.props;
+        if (imgLink) this.props.history.push(imgLink);
+    }
+
     render(){
         const {name, price} = this.props;
         return <>
-        <Grid item xs={12} sm={12} md = {10} >
+        <Grid item xs={12} sm={12} md = {12} >
             <div className = {Style.container}>
-                    <div className = {Style.rec_productContainer} >
-                        <div className = {Style.imgContainer}>
-                        <img ref={this.imgPath} src={this.props.imgPath} alt="shoes" className = {Style.rec_Product}/>
+                    <div ref={this.rec_productContainer}  className = {Style.rec_productContainer} >
+                        <div ref={this.imgContainer} className = {Style.imgContainer}>
+                        <img ref={this.imgPath} src={this.props.imgPath} alt="shoes" className = {Style.rec_Product} onClick={() => this.imgLink()}/>
                         </div>
                         <div className = {Style.rec_productInfo}>
-                            <h4 className = {Style.mainrec_Name}>{name}</h4>
-                            <h4 className = {Style.mainrec_Price}>$ {price}</h4>
-                            <Button variant="outlined" className = {Style.mainrec_Button}>GET IT NOW<ArrowRightIcon /></Button>
+                            <h4 ref={this.mainrec_Name} className = {Style.mainrec_Name}>{name}</h4>
+                            <h4 ref={this.mainrec_Price} className = {Style.mainrec_Price}>$ {price}</h4>
+                            <Button ref={this.mainrec_Button} variant="outlined" className = {Style.mainrec_Button} onClick={() => this.buttonLink()} >GET IT NOW<ArrowRightIcon /></Button>
                         </div>
                     </div>
             </div>
@@ -39,4 +69,6 @@ class MainRectangleCard1 extends React.Component{
 
 }
 
-export default MainRectangleCard1;
+const MainRectangleCard1 = withRouter(_MainRectangleCard1);
+
+export { MainRectangleCard1 };
--- src/pages/User/Product/index.js
+++ src/pages/User/Product/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Style from "./style.module.scss"
-import CategorySquareCard1 from "../../../components/Display/CategorySquareCard1/index"
-import ProductRectangleCard1 from "../../../components/Display/ProductRectangleCard1/index"
-import ProductRectangleCard2 from "../../../components/Display/ProductRectangleCard2/index"
-import MainRectangleCard1 from "../../../components/Display/MainRectangleCard1/index"
+import {CategorySquareCard1} from "../../../components/Display/CategorySquareCard1"
+import {ProductRectangleCard1} from "../../../components/Display/ProductRectangleCard1"
+import {ProductRectangleCard2} from "../../../components/Display/ProductRectangleCard2"
+import {MainRectangleCard1} from "../../../components/Display/MainRectangleCard1"
 function Product() {
     return (
         <>
@@ -18,6 +18,8 @@ function Product() {
                     price = "340"
                     rate = "3.4"
                     numofColor = "8"
+                    buttonLink="/home"
+                    imgLink="/home"
                 />
                 <CategorySquareCard1 
                     name = "Ultraboost 20 Shoes"
@@ -26,6 +28,8 @@ function Product() {
                     rate = "4.6"
                     numofColor = "3"
                     newContainerStyle = "background: rgba(255, 148, 56, 0.2)"
+                    buttonLink="/home"
+                    imgLink="/home"
                 />
                 <CategorySquareCard1 
                     name = "Ultraboost 20 Shoes"
@@ -33,26 +37,32 @@ function Product() {
                     price = "330"
                     rate = "3.8"
                     numofColor = "2"
+                    buttonLink="/home"
+                    imgLink="/home"
                 />
 
-
-                {/* <ProductRectangleCard1 
+{/* 
+                <ProductRectangleCard1 
                     name = "Red Bag Awesome"
                     imgPath={require("../../../images/shoes/zx-2k-4d-shoes.png")}
                     price = "20.0"
                     rate = "4.5"
+                    imgLink="/home"
                 />
                 <ProductRectangleCard1 
                     name = "Red Bag Awesome"
                     imgPath={require("../../../images/shoes/zx-2k-4d-shoes.png")}
                     price = "20.0"
                     rate = "4.5"
+                    imgLink="/home"
                 />
                 <ProductRectangleCard1 
                     name = "Red Bag Awesome"
                     imgPath={require("../../../images/shoes/zx-2k-4d-shoes.png")}
                     price = "20.0"
                     rate = "4.5"
+                    newrec_ContainerStyle = "background:rgba(191, 235, 172, 0.4);"
+                    imgLink="/home"
                 /> */}
                 
                 {/* <ProductRectangleCard2 
@@ -61,7 +71,9 @@ function Product() {
                 price = "185"
                 rate = "3.4"
                 numofColor = "4"
-                
+                newimg_ContainerStyle = "background:rgba(191, 235, 172, 0.4);"
+                buttonLink="/home"
+                imgLink="/home"
                 /> 
 
                 <ProductRectangleCard2 
@@ -70,7 +82,8 @@ function Product() {
                 price = "185"
                 rate = "4.6"
                 numofColor = "8"
-                
+                buttonLink="/home"
+                imgLink="/home"
                 /> 
 
                 <ProductRectangleCard2 
@@ -79,13 +92,17 @@ function Product() {
                 price = "185"
                 rate = "3.8"
                 numofColor = "7"
-                
+                buttonLink="/home"
+                imgLink="/home"
                 />  */}
 
                 {/* <MainRectangleCard1 
                 name = "NIKE BLAZER MID"
                 imgPath={require("../../../images/shoes/zx-2k-4d-shoes.png")}
                 price = "260"
+                newrec_ButtonStyle = "background: rgba(255, 148, 56, 0.2)"
+                buttonLink="/home"
+                imgLink="/home"
                 /> */}
 
 
