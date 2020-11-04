70fb96486f2edee7e1824b8dc23b9add1ce91dc6
connie.yu <imconnieyu@gmail.com>
2020-11-04 17:32:47 +1000

    Fixed menu font size, fixed submenu underline bug, added pointer

--- src/components/Display/NavBar/index.js
+++ src/components/Display/NavBar/index.js
@@ -3,67 +3,81 @@ import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 // import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faShoppingBag,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 import Style from './style.module.scss';
 
 const RenderNavLinks = props => {
   return (
-    <ul
+    <div
       className={
         props.open
-          ? [Style.navLinks, Style.navLinksOpen].join(' ')
-          : [Style.navLinks, Style.navLinksClosed].join(' ')
+          ? [Style.menu__section, Style.menuSectionOpen].join(' ')
+          : [Style.menu__section, Style.menuSectionClosed].join(' ')
       }
     >
-      <li id={Style.menuOne}>
-        <a class={Style.menuLink}>Most Popular</a>
-        <div id={Style.subMenuOne} className={Style.subMenuWrapper}>
-          <ul className={Style.subMenuItem}>
-            <li>
-              <a>Lifestyle</a>
-            </li>
-            <li>
-              <a>Running</a>
-            </li>
-            <li>
-              <a>Basketball</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li id={Style.menuTwo}>
-        <a class={Style.menuLink}>Men</a>
-        <div id={Style.subMenuTwo} className={Style.subMenuWrapper}>
-          <ul className={Style.subMenuItem}>
-            <li>
-              <a>Lifestyle</a>
-            </li>
-            <li>
-              <a>Running</a>
-            </li>
-            <li>
-              <a>Basketball</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-      <li id={Style.menuThree}>
-        <a class={Style.menuLink}>Women</a>
-        <div id={Style.subMenuThree} className={Style.subMenuWrapper}>
-          <ul className={Style.subMenuItem}>
-            <li>
-              <a>Lifestyle</a>
-            </li>
-            <li>
-              <a>Running</a>
-            </li>
-            <li>
-              <a>Basketball</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-    </ul>
+      <ul className={Style.menuList}>
+        <li id={Style.menuOne}>
+          <a class={Style.menuLink}>Most Popular</a>
+          <div id={Style.subMenuOne} className={Style.subMenuWrapper}>
+            <ul className={Style.subMenuList}>
+              <li>
+                <a>Lifestyle</a>
+              </li>
+              <li>
+                <a>Running</a>
+              </li>
+              <li>
+                <a>Basketball</a>
+              </li>
+            </ul>
+          </div>
+          {/* <FontAwesomeIcon icon={faAngleRight} className={Style.angleIcon} /> */}
+        </li>
+        <li id={Style.menuTwo}>
+          <a class={Style.menuLink}>Men</a>
+          <div id={Style.subMenuTwo} className={Style.subMenuWrapper}>
+            <ul className={Style.subMenuList}>
+              <li>
+                <a>Lifestyle</a>
+              </li>
+              <li>
+                <a>Running</a>
+              </li>
+              <li>
+                <a>Basketball</a>
+              </li>
+            </ul>
+          </div>
+          {/* <FontAwesomeIcon icon={faAngleRight} className={Style.angleIcon} /> */}
+        </li>
+        <li id={Style.menuThree}>
+          <a class={Style.menuLink}>Women</a>
+          <div id={Style.subMenuThree} className={Style.subMenuWrapper}>
+            <ul className={Style.subMenuList}>
+              <li>
+                <a>Lifestyle</a>
+              </li>
+              <li>
+                <a>Running</a>
+              </li>
+              <li>
+                <a>Basketball</a>
+              </li>
+            </ul>
+          </div>
+          {/* <FontAwesomeIcon icon={faAngleRight} className={Style.angleIcon} /> */}
+        </li>
+      </ul>
+      <div className={Style.joinMessage}>
+        Become our member for the best products, inspiration and stories in
+        Sports.
+        <a> Learn more</a>
+      </div>
+    </div>
   );
 };
 
@@ -89,18 +103,18 @@ function _NavBar() {
         <div className={Style.navbar__top__item}>Sign In</div>
       </div>
       <div className={Style.navbar__bottom}>
-        <div className={Style.logo}>
-          <img src="favicon.ico" className={Style.logo__image} />
+        <div className={Style.logo__section}>
+          <img src="favicon.ico" className={Style.logo} />
         </div>
         <RenderNavLinks open={open} />
-        <div className={Style.navIcons}>
+        <div className={Style.icon__section}>
           <ShowBurgerMenu open={open} setOpen={setOpen} />
           <a>
             <FontAwesomeIcon icon={faShoppingBag} />
           </a>
         </div>
       </div>
-      <div className={Style.banner}></div>
+      <div className={Style.banner__section}></div>
     </div>
   );
 }
