50023398e720c27fedf448fb805e38e3b275b7d3
Chenxi Li <sunshinerxx@gmail.com>
2020-11-04 21:20:23 +1000

    resolve conflicts about pull request

---
+++ src/components/Display/CartListComponent/CartListCard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Style from './style.module.scss'
+
+class CartListCard extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        const {imgPath, title, size, price} = this.props
+        return <div className={Style["cart__card"]}>
+            <div className={Style["cart__card--img"]}>
+                <figure>
+                    <img src={imgPath}/>
+                </figure>
+            </div>
+            <div className={Style["cart__card--desc"]}>
+                <div className={Style["cart__card--container"]}>
+                <h3 className={Style["cart__card--title"]}>{title}</h3>
+                    <h3 className={Style["cart__card--size"]}>{size}</h3>
+                </div>
+            </div>
+            <div className={Style["cart__card--price"]}>
+                <h3>{price}</h3>
+            </div>
+        </div>
+    }
+}
+
+export default CartListCard
